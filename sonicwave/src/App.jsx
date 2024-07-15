0f5cfba21ec6941a24fb526c13839df70e29e17b
Bharath-kukkunoor <bharathkukkunoor@gmail.com>
2024-07-15 16:25:20 +0530

    eusjs

--- sonicwave/src/App.jsx
+++ sonicwave/src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Sidebar from './Components/Sidebar'
 import Player from './Components/Player'
 import Display from './Components/Display'
+import { PlayerContext } from './context/PlayerContext'
 
 
 
 const App = () => {
+  const audioRef= useContext(PlayerContext);
+  const track= useContext(PlayerContext);
+
+
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex '>
@@ -13,9 +18,12 @@ const App = () => {
       <Display/>
       </div>
       <Player/>
-      <audio preload='auto'></audio>
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
   )
 }
 
 export default App
+
+
+
